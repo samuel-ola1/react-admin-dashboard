86291bcf6da83623ebc32e4175d8ffc3545d0b89
Samuel Olayande <samuelolayande@gmail.com>
2022-11-07 16:36:47 +0100

    working on topbar

--- src/scenes/global/Topbar.jsx
+++ src/scenes/global/Topbar.jsx
@@ -1,6 +1,35 @@
+import {Box, IconButton, useTheme} from "@mui/material"
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+
+
+
 const Topbar = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const colorMode = useContext(ColorModeContext);
+
     return (
-        <div>Topbar</div>
+       <Box display="flex" justifyContent="space-between" p={2}>
+        {/* search bar */}
+
+        <Box
+            display="flex"
+            backgroundColor={colors.primary[400]}
+            borderRadius="3px"
+        >
+            <InputBase sx={ {ml: 2, flex: 1}} placeholder="Search" />
+        </Box>
+        <IconButton></IconButton>
+       </Box>
     );
 }
  
